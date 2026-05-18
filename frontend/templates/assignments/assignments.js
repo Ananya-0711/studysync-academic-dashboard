de1ea3f6fa0645ad86cb4cdb83ab162dd151162b
Ananya-0711 <ananya.vadige@gmail.com>
2026-05-18 23:05:32 +0530

    Restructured assignment module

--- frontend/templates/assignments/assignments.js
+++ frontend/templates/assignments/assignments.js
@@ -170,7 +170,6 @@ function closeAboutModal(){
 }
 
 
-
 /* CLOSE ON OUTSIDE CLICK */
 
 window.addEventListener("click",(e)=>{
@@ -185,7 +184,6 @@ window.addEventListener("click",(e)=>{
   }
 
 });
-
 
 
 /* CLOSE ON ESC */
